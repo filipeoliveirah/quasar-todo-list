6e2f6fa64a10324f7ed51cebff7e90dde150e9d4
Filipe Oliveira <t-filipe.oliveira@matrixenergia.com>
2023-11-16 15:46:23 -0300

    feat: style MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,6 @@
     <q-header elevated>
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
-
         <q-toolbar-title>
           Menu
         </q-toolbar-title>
@@ -12,16 +11,20 @@
         <div class="text-h3">ToDo List</div>
         <span class="text-subtitle1">{{ todaysDate }}</span>
       </div>
-      <q-img src="../assets/blurred-header.jpg" class="header-image absolute-top" />
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label header>
-          Essential Links
-        </q-item-label>
+        <q-img src="../assets/blurred-header.jpg" style="height: 247px">
+          <div class="absolute-bottom absolute-center bg-transparent">
+            <q-avatar size="96px" class="q-mb-sm">
+              <img src="https://avatars.githubusercontent.com/u/26912630?v=4">
+            </q-avatar>
+            <div class="text-weight-bold text-h5 q-mb-sm">Filipe M.</div>
+          </div>
+        </q-img>
 
-        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
+        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" :target="link.target" />
       </q-list>
     </q-drawer>
 
@@ -37,47 +40,17 @@ import EssentialLink from 'components/EssentialLink.vue';
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: 'Tarefas',
+    icon: 'list',
+    link: '/',
+    target: '',
   },
   {
     title: 'Github',
-    caption: 'github.com/quasarframework',
     icon: 'code',
-    link: 'https://github.com/quasarframework'
+    link: 'https://github.com/filipeoliveirah',
+    target: '_blank',
   },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
 ];
 
 export default defineComponent({
